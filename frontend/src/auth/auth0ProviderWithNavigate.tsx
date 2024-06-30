da71e0da97de5c1d786b01d0fdcac534049a27dc
ANSHUL2122 <anshulmakhija125@gmail.com>
2024-06-30 13:48:28 +0530

    Auth0 Access Token created

--- frontend/src/auth/auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/auth0ProviderWithNavigate.tsx
@@ -1,26 +1,24 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode;
 }
 
 const auth0ProviderWithNavigate = ({ children }: Props) => {
-    const { createUser } = useCreateMyUser();
+    
     // auth things here
     const domian = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const navigate = useNavigate();
     if (!domian || !clientId || !redirectUri || !redirectUri) {
         throw new Error(`unable to initialize auth`);
     }
 
     //redirect to user to app after auth successful
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        if (user?.sub && user?.email) {
-            //whenever user sign in its creates a user in database
-            createUser({ auth0Id: user.sub, email: user.email });
-        }
+        navigate("/auth-callvback")
     }
     return (
         <Auth0Provider
